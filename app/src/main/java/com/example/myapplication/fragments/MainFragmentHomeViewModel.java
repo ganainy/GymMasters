172aefd665f16bec0b1f9afc0cfdc68be8bc7e64
amr <afoda50@gmail.com>
2019-10-12 17:25:50 +0200

    home fragment layout update

---
+++ app/src/main/java/com/example/myapplication/fragments/MainFragmentHomeViewModel.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainFragmentHomeViewModel extends ViewModel {
+
+    private static final String TAG = "MainFragmentHomeViewMod";
+
+
+}
